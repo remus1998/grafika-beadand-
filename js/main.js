704dbb7562e62da63c63458c7d8dec011476bfb5
Remus <mprekuta@gmail.com>
2021-10-11 21:48:20 +0200

    minor changes

--- js/main.js
+++ js/main.js
@@ -134,7 +134,9 @@ function reset(){
     document.getElementById('first').style.backgroundImage = "none";
 }window.addEventListener('load',reset)
 
-
+function main(){
+    window.open('https://remus1998.github.io/ConverterGatherer/','_blank') 
+  }window.addEventListener('load',main)
 
 
   
